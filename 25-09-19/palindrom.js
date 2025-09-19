82fbfcb2171f8544beca98bc36bc3282b30a4566
ibrahim-thoughtworks <mydrive1a@gmail.com>
2025-09-19 13:07:55 +0530

    Created palindrom using == and string

---
+++ 25-09-19/palindrom.js
@@ -0,0 +1,12 @@
+const number = 1000;
+let numberLength = (number+"").length;
+let revText = "";
+
+for( let i=0; i< (numberLength) ; i++) {
+    revText = revText + (number+"")[numberLength-1-i];
+}
+if (number == revText) {
+    console.log("palindrom",number);
+} else {
+    console.log("Not palindrom ",number);
+}
